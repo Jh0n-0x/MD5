4594839e5da1c735951aa87e89574c7b3ca3cb10
Pablo Santhus <santhuspablo@gmail.com>
2016-08-20 23:15:43 -0300

    Update md5.php

--- md5.php
+++ md5.php
@@ -1,15 +1,11 @@
 <?php
-
   /*
     coded by Pablo Santhus
     09/08/2016
   */
-
 	error_reporting(0);
 	echo menu();
-
 	function menu(){
-
 print"\n";
 echo"		 (                         (                                 \n";     
 echo" )\ )          )  (        )\ )                 )    )            	 \n";
@@ -24,17 +20,12 @@ echo "Encriptar MD5 \n";
 echo "Coded By Pablo Santhus \n";
 echo "exemplo: php md5.php suasenha \n";
 }
-
-	function encriptar(){
-		$encrypt = md5($argv[1]);
-		return $encrypt;
-	}
-
+	$encrypt = md5($argv[1]);
 	if(isset($argv[1])){
 		print "\n";
 		print "\n";
 		print "--------------------------------------------------------- \n";
-		echo encriptar();
+		echo $encrypt;
 		print "\n";
 		print "---------------------------------------------------------\n";
 		print "\n";
